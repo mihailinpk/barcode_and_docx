7641cd15f456db67794cd62783c460436374be15
petr.mikhailin <petr.mikhailin@givc.mkrf.ru>
2022-02-18 16:39:12 +0300

    generation *.docx files added

---
+++ src/main/java/su/givc/learningprojects/barcode_and_docx/service/DocumentsGenerateService.java
@@ -0,0 +1,100 @@
+package su.givc.learningprojects.barcode_and_docx.service;
+
+import com.google.zxing.WriterException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.util.Units;
+import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * Класс-компонент DocumentsGenerateService
+ * <p/>
+ * Сервис генерации документов
+ * <p/>
+ *
+ * @author mihailinpk
+ * created 18.02.2022 12:28
+ */
+@Component
+public class DocumentsGenerateService {
+
+    /**
+     * Текст для заголовка документа
+     */
+    @Value("${application.docx.title.text}")
+    private String titleDocxText;
+
+    /**
+     * Имя генерируемого файла
+     */
+    @Value("${application.docx.filename}")
+    private String fileName;
+
+    /**
+     * Генерация штрих-кодов
+     */
+    private final BarcodeGenerateService barcodeGenerateService;
+
+    public DocumentsGenerateService(BarcodeGenerateService barcodeGenerateService) {
+        this.barcodeGenerateService = barcodeGenerateService;
+    }
+
+    /**
+     * Генерация *.docx-документа, вставка в него переданного в параметре текста и генерация и вставка в конец
+     * документа QR-кода, генерируемого из переданного текста
+     *
+     * @param text текст для вставки в сгенерированный *.docx-документ и для генерации QR-кода для вставки в конец
+     *             этого же документа
+     */
+    public void generateWordDocument(String text) throws Exception {
+
+        XWPFDocument document = new XWPFDocument();
+        XWPFParagraph titleParagraph = document.createParagraph();
+        titleParagraph.setAlignment(ParagraphAlignment.CENTER);
+        XWPFRun titleRun = titleParagraph.createRun();
+        titleRun.setText(titleDocxText);
+        titleRun.setBold(true);
+        titleRun.setFontFamily("Times New Roman");
+        titleRun.setFontSize(20);
+
+        XWPFParagraph mainTextParagraph = document.createParagraph();
+        XWPFRun mainTextRun = mainTextParagraph.createRun();
+        mainTextRun.setText(text);;
+        mainTextRun.setFontFamily("Times New Roman");
+        mainTextRun.setTextPosition(20);
+        mainTextRun.setFontSize(14);
+
+        BufferedImage qrCode = barcodeGenerateService.generateQRcodeImage(text);
+        FileOutputStream fileQrOutputStream = new FileOutputStream("qr.png");
+        ImageIO.write(qrCode, "png", fileQrOutputStream);
+        fileQrOutputStream.flush();
+        fileQrOutputStream.close();
+        FileInputStream fileInputStream = new FileInputStream("qr.png");
+
+        XWPFParagraph qrCodeParagraph = document.createParagraph();
+        qrCodeParagraph.setAlignment(ParagraphAlignment.CENTER);
+        XWPFRun qrCodeRun = qrCodeParagraph.createRun();
+        qrCodeRun.setTextPosition(20);
+        qrCodeRun.addPicture(fileInputStream, XWPFDocument.PICTURE_TYPE_PNG, "qr.png", Units.toEMU(100), Units.toEMU(100));
+
+        FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+        document.write(fileOutputStream);
+        fileInputStream.close();
+        fileOutputStream.close();
+        document.close();
+        Files.delete(Paths.get("qr.png"));
+
+    }
+
+}
--- src/test/java/su/givc/learningprojects/barcode_and_docx/BarcodeAndDocxApplicationTests.java
+++ src/test/java/su/givc/learningprojects/barcode_and_docx/BarcodeAndDocxApplicationTests.java
@@ -1,13 +1,39 @@
 package su.givc.learningprojects.barcode_and_docx;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import su.givc.learningprojects.barcode_and_docx.service.DocumentsGenerateService;
 
 @SpringBootTest
 class BarcodeAndDocxApplicationTests {
 
+	DocumentsGenerateService documentsGenerateService;
+
+	@Autowired
+	public BarcodeAndDocxApplicationTests(DocumentsGenerateService documentsGenerateService) {
+		this.documentsGenerateService = documentsGenerateService;
+	}
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void generateDocxDocument()	{
+		String text = "It's a long way to Tipperary,\n" +
+				"It's a long way to go;\n" +
+				"It's a long way to Tipperary,\n" +
+				"And the sweetest girl I know.\n" +
+				"Good-bye, Piccadilly,\n" +
+				"Farewell, Lester Square:\n" +
+				"It's a long, long way to Tipperary,\n" +
+				"But my heart's right there.";
+		try {
+			documentsGenerateService.generateWordDocument(text);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
